385740f4362acb3c9b87f590b1b21e867e852de1
Nguyễn Phạm Hùng Nhân <thisisaseacow@gmail.com>
2025-10-30 21:54:41 +0700

    thêm trang user

---
+++ main-app/app/src/main/java/com/example/spotixe/Pages/Pages/AppMainPages/User.kt
@@ -0,0 +1,150 @@
+package com.example.spotixe.Pages.Pages.AppMainPages
+
+import Components.ExploreSection
+import Components.GenresSection
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.spotixe.Data.genres
+import com.example.spotixe.Data.recentlyPlayed
+import com.example.spotixe.Data.topPicks
+import com.example.spotixe.R
+
+@Composable
+fun UserScreen(
+    navController: NavController,
+){
+
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxSize()
+        ) {
+            // Header
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp, start = 8.dp, end = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier
+                        .padding(start = 8.dp)
+                        .size(40.dp)
+                        .clickable { navController.popBackStack() },
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBackIosNew,
+                        contentDescription = "Back",
+                        tint = Color(0xFF58BA47),
+                        modifier = Modifier
+                            .size(40.dp)
+                            .padding(6.dp)
+                    )
+                }
+                Spacer(Modifier.width(110.dp))
+                Text(
+                    text = "User",
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF58BA47),
+                    textAlign = TextAlign.Center
+                )
+            }
+
+            Spacer(Modifier.height(4.dp))
+
+            Divider(color = Color.White.copy(alpha = 0.2f))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(350.dp)
+                    .background(
+                        Brush.linearGradient(
+                            listOf(
+                                Color(0xFF000000),
+                                Color(0xFF031508).copy(alpha = 0.7f),
+                                Color(0xFF58BA47).copy(alpha = 0.9f)
+                            ),
+                            start = Offset(600f, 0f),
+                            end   = Offset(0f, 800f)
+                        )
+                    ),
+                contentAlignment = Alignment.Center
+            ){
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Image(
+                        painter = painterResource(R.drawable.spotixe_logo),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(200.dp)
+                    )
+
+                    Button(
+                        onClick = {},
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFF545252),
+                        )
+                    ) {
+                        Text(
+                            "Sửa hồ sơ",
+                            color = Color.White
+                        )
+                    }
+                }
+
+            }
+        }
+    }
+}
