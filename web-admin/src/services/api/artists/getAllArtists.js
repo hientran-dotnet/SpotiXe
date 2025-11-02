1835a4f23e2d659ba0a7d0e917d165c790762643
HienTran <hientm0978@gmail.com>
2025-11-02 20:45:14 +0700

    Thêm api lấy toàn bộ danh sách thông tin nghệ sĩ

--- web-admin/src/services/api/artists/getAllArtists.js
+++ web-admin/src/services/api/artists/getAllArtists.js
@@ -1,6 +1,10 @@
 import axios from "../axiosInstance";
 
+/**
+ * Fetch all artists from the API
+ * @returns {Promise<Array>} List of artists
+ */
 export async function getAllArtists() {
-  const res = await axios.get("/artists");
-  return res.data?.items ?? [];
+  const res = await axios.get("/Artists");
+  return res.data?.items ?? res.data ?? [];
 }
